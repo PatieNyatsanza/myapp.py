f844029c87c59faa77001597f37d2d10592f8a83
PatieNyatsanza <86954566+PatieNyatsanza@users.noreply.github.com>
2021-08-07 17:52:15 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -8,13 +8,13 @@ Shown are the **stock closing price** and **volume** of Google!""" )
 
 # https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
 # define the ticker symol
- tickerSymbol ='GOOGL'
- # GET DATA ON THIS ticker
- tickerData = yf.Ticker(tickerSymbol)
- # get the historical prices for this tickerData
- tickerDf = tickerData.history(period='1d', start='2010-5-31', ends='2020-5-31')
+tickerSymbol = 'GOOGL'
+# GET DATA ON THIS ticker
+tickerData = yf.Ticker(tickerSymbol)
+# get the historical prices for this tickerData
+tickerDf = tickerData.history(period='1d', start='2010-5-31', ends='2020-5-31')
 
- #Open High Low Close Volume Dividends   Stock Splits
+#Open High Low Close Volume Dividends   Stock Splits
 
- st.line_chart(tickerDf.Close)
- st.line_chart(tickerDf.Volume)
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
